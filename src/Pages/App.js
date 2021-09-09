ad800637f5f9a83c1a603737459d03274cd3df79
saeedshari <saeed.sharipoor@gmail.com>
2021-09-09 12:32:48 +0430

    Create custom hook for theme mode

--- src/Pages/App.js
+++ src/Pages/App.js
@@ -3,15 +3,29 @@ import './App.css';
 import Products from '../Components/Products/Products';
 import Auth from '../Components/UI/Auth';
 import { AuthContext } from '../context/auth-context';
+import useDarkMode from '../hooks/dark-mode';
 
 const App = ()=> {
 
     const authContext = useContext(AuthContext)
 
+    const [theme, toggleTheme] = useDarkMode()
+
     let content = <Auth/>
 
     if(authContext.isLogin){
-        content = <Products/>
+        content = (
+            <div className="app" 
+            style={{
+                background: theme === "dark" ? "#282828" : "#ffffff",
+                color: theme === "dark" ? "#ffffff" : "#000",
+                textAlign: "center",
+                transition: "0.3s all",
+            }}>
+                <button onClick={toggleTheme}>تغییر تم</button>
+                <Products/>
+            </div>
+        )
     }
 
     return content
